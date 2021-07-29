3966a99c4a096a1e417afa027fb98866d05f18ec
LindaS46 <zhonghuis2020@gmail.com>
2021-07-29 15:17:41 -0700

    add Doctor class

---
+++ app/src/main/java/com/example/clinicappproject/Doctor.java
@@ -0,0 +1,11 @@
+package com.example.clinicappproject;
+
+import java.util.List;
+
+public class Doctor {
+    public String name;
+    public String gender;
+    public String specialization;
+//    public List<Patient> visted_patients;
+
+}
