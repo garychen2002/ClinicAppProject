ee3a7ef6a023e609aec4e7ec57a4ce434aa25e4c
LindaS46 <zhonghuis2020@gmail.com>
2021-07-30 13:46:11 -0700

    Create Doctor's layout

--- app/src/main/java/com/example/clinicappproject/Doctor.java
+++ app/src/main/java/com/example/clinicappproject/Doctor.java
@@ -3,9 +3,19 @@ package com.example.clinicappproject;
 import java.util.List;
 
 public class Doctor {
-    public String name;
-    public String gender;
-    public String specialization;
-//    public List<Patient> visted_patients;
+    private String name;
+    private String gender;
+    private String specialization;
+    private List<Patient> visted_patients;
+
+    String getName(){
+        return this.name;
+    }
+
+    String getGender(){
+        return this.gender;
+    }
+
+
 
 }
