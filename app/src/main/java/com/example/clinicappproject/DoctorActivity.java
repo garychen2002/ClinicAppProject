03426b8862e5d8c15a413370f24b53cb0b4ec1ea
LindaS46 <zhonghuis2020@gmail.com>
2021-07-31 15:02:46 -0700

    Changed DoctorActivity

--- app/src/main/java/com/example/clinicappproject/DoctorActivity.java
+++ app/src/main/java/com/example/clinicappproject/DoctorActivity.java
@@ -1,22 +1,22 @@
 package com.example.clinicappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
 
 public class DoctorActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
     }
-
-    public void openViewAppointment(){
-//        Intent intent = new Intent(this, DisplayAppointmentActivity.class);
-//        startActivity(intent);
+    
+    public void openUpcomingAppointment(View view){
+        Intent intent = new Intent(this, DisplayAppointmentActivity.class);
+        startActivity(intent);
     }
 
 }
