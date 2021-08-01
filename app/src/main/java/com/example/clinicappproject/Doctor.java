eb6daf0756371a4457a2362d0d6a41e153257011
Gary Chen <realgarychen@gmail.com>
2021-07-31 21:26:38 -0400

    Add serializable functionality to classes
    we need classes to have getters/setters/empty constructor/serializable to work with firebase's getValue

--- app/src/main/java/com/example/clinicappproject/Doctor.java
+++ app/src/main/java/com/example/clinicappproject/Doctor.java
@@ -1,9 +1,10 @@
 package com.example.clinicappproject;
 
+import java.io.Serializable;
 import java.util.LinkedHashSet;
 import java.util.List;
 
-public class Doctor extends User{
+public class Doctor extends User implements Serializable {
     private String username;
     private String passwork;
     private String name;
@@ -11,6 +12,10 @@ public class Doctor extends User{
     private String specialization;
     private List<Patient> visted_patients;
     private List<Appointment> appointments;
+
+    public Doctor() {
+
+    }
 
     public Doctor(String user_n, String p, String n, String g, String sp, List<Patient> v_p, List<Appointment> appointment){
         super(user_n, p, n, g);
@@ -47,5 +52,52 @@ public class Doctor extends User{
         return "Gender " + this.gender + "specialization: " + this.specialization;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswork() {
+        return passwork;
+    }
+
+    public void setPasswork(String passwork) {
+        this.passwork = passwork;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public List<Patient> getVisted_patients() {
+        return visted_patients;
+    }
+
+    public void setVisted_patients(List<Patient> visted_patients) {
+        this.visted_patients = visted_patients;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
 }
 
--- app/src/main/java/com/example/clinicappproject/User.java
+++ app/src/main/java/com/example/clinicappproject/User.java
@@ -1,12 +1,17 @@
 package com.example.clinicappproject;
 
+import java.io.Serializable;
 import java.util.List;
 
-public abstract class User {
+public abstract class User implements Serializable {
     private String username;
     private String password;
     private String name;
     private String gender;
+
+    public User() {
+
+    }
 
     public User(String user_n, String p, String n, String g){
         username = user_n;
