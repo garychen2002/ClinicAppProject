e2457ba2a2604cfec87078534bd57094b5f59da1
Gary Chen <realgarychen@gmail.com>
2021-07-31 21:38:18 -0400

    Revert "Add serializable functionality to classes"
    This reverts commit eb6daf0756371a4457a2362d0d6a41e153257011.

--- app/src/main/java/com/example/clinicappproject/User.java
+++ app/src/main/java/com/example/clinicappproject/User.java
@@ -1,17 +1,12 @@
 package com.example.clinicappproject;
 
-import java.io.Serializable;
 import java.util.List;
 
-public abstract class User implements Serializable {
+public abstract class User {
     private String username;
     private String password;
     private String name;
     private String gender;
-
-    public User() {
-
-    }
 
     public User(String user_n, String p, String n, String g){
         username = user_n;
