c736c6c1e3b2648ce00789adb7fac0b73a1685bf
MeganMujiaLiu <megan13436702763@gmail.com>
2021-08-06 14:51:02 +0800

    set DatePicker for patient

--- app/src/main/java/com/example/clinicappproject/ChooseUpcomingWeekTime.java
+++ app/src/main/java/com/example/clinicappproject/ChooseUpcomingWeekTime.java
@@ -2,18 +2,53 @@ package com.example.clinicappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class ChooseUpcomingWeekTime extends AppCompatActivity {
 
     Doctor final_doctor;
     Patient current_patient;
     Appointment appointment;
+    Spinner dateList;
+    Spinner timeList;
+    ArrayAdapter<String> dateAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Intent intent= getIntent();
+        current_patient = UserSingleton.getInstance().getCurrentPatient();
+        final_doctor =(Doctor) intent.getSerializableExtra("FINAL_DOCTOR");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_upcoming_week_time);
+
+        DatePicker dp = findViewById(R.id.DatePicker);
+        upcomingWeek(dp);
+
+
+    }
+
+
+
+
+
+    public void upcomingWeek(DatePicker dp){
+        GregorianCalendar current = new GregorianCalendar();
+        current.add(GregorianCalendar.DATE,1);
+        dp.setMinDate(current.getTimeInMillis());
+        current.add(GregorianCalendar.DATE,6);
+        dp.setMaxDate(current.getTimeInMillis());
+
 
     }
 }
