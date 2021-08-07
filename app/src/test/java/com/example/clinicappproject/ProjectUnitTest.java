32c0a8b2aba7fc854a0e235e9dfd94c72807b5ed
Gary Chen <realgarychen@gmail.com>
2021-08-07 00:46:55 -0400

    Add initial unit tests
    wip

---
+++ app/src/test/java/com/example/clinicappproject/ProjectUnitTest.java
@@ -0,0 +1,80 @@
+package com.example.clinicappproject;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ProjectUnitTest {
+    @Mock
+    PatientLoginTest patientView;
+
+    @Mock
+    DoctorLoginTest doctorView;
+
+    @Mock
+    Model model;
+
+    @Test
+    public void testCorrectPatient() {
+        when(patientView.getUsername()).thenReturn("test1");
+        when(patientView.getPassword()).thenReturn("password1");
+        Patient testPatient = new Patient();
+        testPatient.setUsername("test1");
+        testPatient.setPassword("password1");
+        Presenter presenter = new Presenter(model, patientView);
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((Presenter) invocation.getArguments()[0]).onSuccess(testPatient);
+                return null;
+            }
+        }).when(model).patientLogin(presenter, "test1", "password1");
+        presenter.checkPatientCredentials();
+        verify(patientView).onLoginSuccess(testPatient);
+
+
+    }
+
+    @Test
+    public void testIncorrectPatient() {
+        when(patientView.getUsername()).thenReturn("test1");
+        when(patientView.getPassword()).thenReturn("wrongpassword");
+        Patient testPatient = new Patient();
+        testPatient.setUsername("test1");
+        testPatient.setPassword("wrongpassword");
+        Presenter presenter = new Presenter(model, patientView);
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                ((Presenter) invocation.getArguments()[0]).onFailure();
+                return null;
+            }
+        }).when(model).patientLogin(presenter, "test1", "wrongpassword");
+        presenter.checkPatientCredentials();
+        verify(patientView).onLoginFailure();
+
+
+    }
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
